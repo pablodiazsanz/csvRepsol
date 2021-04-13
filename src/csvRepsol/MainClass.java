798772b7702eb6dd04725ff3afa860c36faca619
pdiazs <pdiazs@11LAP5CG7496KY6.indra.es>
2021-04-13 17:06:06 +0200

    Maven creado y log4j funcionando

--- src/csvRepsol/MainClass.java
+++ src/csvRepsol/MainClass.java
@@ -2,9 +2,18 @@ package csvRepsol;
 
 import java.util.HashMap;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
 public class MainClass {
+	
+	static Logger log = Logger.getLogger(MainClass.class);
 
 	public static void main(String[] args) {
+        log.debug("Arranca la aplicación");
+        log.info("this is a information log message");
+        log.warn("this is a warning log message");
+		
 		CsvAccess csvAccess = new CsvAccess();
 		csvAccess.createCSV();
 		HashMap<String, Employee> clientData = csvAccess.readCSV("client");
